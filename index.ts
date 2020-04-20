4facbe6e2f5db61750026e055a74dd118db43e69
SimoneTesta1998 <simone3000.st@gmail.com>
2020-04-20 19:14:56 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import { of } from 'rxjs'; 
-import { map, switchMap } from 'rxjs/operators';
+import { of, interval } from 'rxjs'; 
+import { map, switchMap, filter, toArray } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 of(1,3,5).pipe(
-    switchMap(i => of(i * 10, i * 10, i * 10))
+    switchMap(i => 
+    of(i * 10, i * 10, i * 10)
+  ),
+  toArray()
 ).subscribe(console.log)
